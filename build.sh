f79c32e6e4de54ab77772fb083bb4d04b10aea00
Herman Semenov <GermanAizek@yandex.ru>
2024-05-03 19:41:14 -0500

    Continue working on repo

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Title........: build.sh
+#Description..: This is bash script for Linux systems to building from sources latest release version hostapd and patching for facilitate AP impersonation attacks 
+#Author.......: German Semenov <GermanAizek@yandex.ru>
+#Usage........: bash build.sh
+#Bash Version.: 4.2 or later
+
+# You will need packages to build on Debian, Kali Linux, Ubuntu and etc.
+sudo apt install -y libssl-dev libnl-genl-3-dev git build-essential
+
+
+
